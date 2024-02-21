efdeb770621863ac79d28ea4eae47df8384eb4db
Tigran Najaryan <tnajaryan@splunk.com>
2024-02-21 11:55:44 -0500

    Begin adding entities to k8s_cluster receiver

--- receiver/k8sclusterreceiver/internal/metadata/entities.go
+++ receiver/k8sclusterreceiver/internal/metadata/entities.go
@@ -5,6 +5,7 @@ package metadata // import "github.com/open-telemetry/opentelemetry-collector-co
 
 import (
 	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/pentity"
 
 	metadataPkg "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/experimentalmetricmetadata"
 )
@@ -12,6 +13,7 @@ import (
 // GetEntityEvents processes metadata updates and returns entity events that describe the metadata changes.
 func GetEntityEvents(oldMetadata, newMetadata map[metadataPkg.ResourceID]*KubernetesMetadata, timestamp pcommon.Timestamp) metadataPkg.EntityEventsSlice {
 	out := metadataPkg.NewEntityEventsSlice()
+	out2 := pentity.NewEntityEventSlice()
 
 	for id, oldObj := range oldMetadata {
 		if _, ok := newMetadata[id]; !ok {
@@ -20,6 +22,11 @@ func GetEntityEvents(oldMetadata, newMetadata map[metadataPkg.ResourceID]*Kubern
 			entityEvent.SetTimestamp(timestamp)
 			entityEvent.ID().PutStr(oldObj.ResourceIDKey, string(oldObj.ResourceID))
 			entityEvent.SetEntityDelete()
+
+			entityEvent2 := out2.AppendEmpty()
+			entityEvent2.SetTimestamp(timestamp)
+			entityEvent2.Id().PutStr(oldObj.ResourceIDKey, string(oldObj.ResourceID))
+			entityEvent2.SetEmptyEntityDelete()
 		}
 	}
 
@@ -34,6 +41,17 @@ func GetEntityEvents(oldMetadata, newMetadata map[metadataPkg.ResourceID]*Kubern
 		attrs := state.Attributes()
 		for k, v := range newObj.Metadata {
 			attrs.PutStr(k, v)
+		}
+
+		entityEvent2 := out2.AppendEmpty()
+		entityEvent2.SetTimestamp(timestamp)
+		entityEvent2.Id().PutStr(newObj.ResourceIDKey, string(newObj.ResourceID))
+		state2 := entityEvent2.SetEmptyEntityState()
+		entityEvent2.SetEntityType(newObj.EntityType)
+
+		attrs2 := state2.Attributes()
+		for k, v := range newObj.Metadata {
+			attrs2.PutStr(k, v)
 		}
 	}
 
